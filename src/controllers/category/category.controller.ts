3f2c3777baa756d49c0cf831775a382f1f5ab64d
AVex3nder <alex.vila.dev@gmail.com>
2022-01-31 16:32:28 +0100

    testing response object

--- src/controllers/category/category.controller.ts
+++ src/controllers/category/category.controller.ts
@@ -1,16 +1,21 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Post, Res} from '@nestjs/common';
 import {Category} from 'src/entities/category.entity';
 import {CategoryService} from '../../services/category/category.service';
+import {Response} from 'express';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  createCategory(@Body() category: Category) {
-    this.categoryService
-      .insert(category)
-      .then(res => res)
-      .catch(err => err);
+  createCategory(@Body() categoryName: string) {
+    const category = new Category();
+    category.name = categoryName;
+    const response = this.categoryService.insert(category);
+    console.log('controller', response);
+    return response;
   }
+
+  @Delete()
+  deleteCategory(@Body() categoryName: string) {}
 }
--- src/services/category/category.service.ts
+++ src/services/category/category.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
+import {response} from 'express';
 import {Category} from 'src/entities/category.entity';
 import {Repository} from 'typeorm';
 
