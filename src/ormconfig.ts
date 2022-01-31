1002c47bd73caaaeae6c5755da88b2c0cef1ed39
AVex3nder <alex.vila.dev@gmail.com>
2022-01-31 13:54:54 +0100

    debugging start on heroku 12

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,6 +4,9 @@ require('dotenv').config();
 const config: PostgresConnectionOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 export default config;
