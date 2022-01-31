808b6e42ba526f28f9283fa4d325fec79ed5b38f
AVex3nder <alex.vila.dev@gmail.com>
2022-01-31 12:11:01 +0100

    debugging start on heroku 3

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
