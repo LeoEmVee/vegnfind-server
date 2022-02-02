6921d3d5f0b3e1fffe93c1a947468b51cb6a4b8b
AVex3nder <alex.vila.dev@gmail.com>
2022-02-02 19:33:03 +0100

    hot fix cloudinary

--- src/services/cloudinary/cloudinary.service.ts
+++ src/services/cloudinary/cloudinary.service.ts
@@ -1,5 +1,12 @@
-const cloudinary = require('../utils/cloudinary');
 import {Injectable} from '@nestjs/common';
+require('dotenv').config();
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 @Injectable()
 export class CloudinaryService {
