fdd4e31a9089d54d89c0f5ac36ae5d6498a77aa2
AVex3nder <alex.vila.dev@gmail.com>
2022-02-03 15:13:33 +0100

    testing new cors protocol app.enableCors()

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import {AppModule} from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(process.env.PORT || 5000);
