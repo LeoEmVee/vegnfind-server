26ee44d119664ec74b321aa6b0868f28de30b014
AVex3nder <alex.vila.dev@gmail.com>
2022-02-07 09:40:24 +0100

    implemented favourites controller and service

---
+++ src/services/favourites/favourites.service.ts
@@ -0,0 +1,59 @@
+import {ConflictException, NotFoundException, Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Favourites} from '../../entities/favourites.entity';
+import {Repository} from 'typeorm';
+
+@Injectable()
+export class FavouritesService {
+  constructor(
+    @InjectRepository(Favourites)
+    private favouritesRepository: Repository<Favourites>,
+  ) {}
+
+  async findOneByCondition(condition: any): Promise<Favourites> {
+    try {
+      return await this.favouritesRepository.findOneOrFail(condition);
+    } catch (error) {
+      throw new NotFoundException(error, "This Fav doesn't exist");
+    }
+  }
+
+  async createOne(fav: Favourites): Promise<Favourites> {
+    const favExists = await this.favouritesRepository.findOne(null, {
+      where: {user: fav.user},
+    });
+    if (favExists) {
+      throw new ConflictException(
+        favExists,
+        'This user already has a Fav list!',
+      );
+    }
+    const newFav = await this.favouritesRepository.create({...fav});
+    return this.favouritesRepository.save(newFav);
+  }
+
+  async updateOne(fav: Favourites): Promise<Favourites> {
+    const id = fav.id;
+    const oldFav = await this.findOneByCondition({id});
+    const newFav = {...oldFav, ...fav};
+    return this.favouritesRepository.save(newFav);
+  }
+
+  async deleteOneByCondition(condition: any): Promise<Favourites> {
+    try {
+      const fav = await this.findOneByCondition(condition);
+      return await this.favouritesRepository.remove(fav);
+    } catch (error) {
+      throw new NotFoundException(error, "Couldn't delete, entry not found.");
+    }
+  }
+
+  // ONLY FOR DEVELOPMENT
+  async deleteAll(): Promise<void> {
+    await this.favouritesRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Favourites)
+      .execute();
+  }
+}
